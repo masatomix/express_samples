d94a8b3730749206d2f5064efa33614ca7f5bfa8
Masatomi KINO <masatomix@ki-no.org>
2019-02-02 21:48:48 +0900

    promise-mysqlへ載せ替え。 ついでにTransactionも。まだ未確認。

--- src/userRouter.ts
+++ src/userRouter.ts
@@ -1,12 +1,10 @@
 import * as express from "express";
-import * as mysql from "mysql";
-import * as util from "util";
+import * as mysql from "promise-mysql";
 import config from "./config";
 
 const router = express.Router();
 
 let mysqlPool = mysql.createPool(config);
-mysqlPool.query = util.promisify(mysqlPool.query);
 
 router
   // findAll
@@ -14,8 +12,9 @@ router
   // 結果配列を返す
   .get("/", async (req, res) => {
     console.log("find all user start.");
+    let connection = await mysqlPool.getConnection()
     try {
-      const rows = await mysqlPool.query("select * from  USER_MASTER;");
+      const rows = await connection.query("select * from  USER_MASTER;");
       if (rows.length === 0) {
         res.status(404).send();
         return;
@@ -26,6 +25,7 @@ router
       res.status(500).send(err);
     } finally {
       console.log("find all user end.");
+      mysqlPool.releaseConnection(connection);
     }
   })
 
@@ -37,18 +37,23 @@ router
     const user = req.body;
     console.log(JSON.stringify(user));
 
+    let connection = await mysqlPool.getConnection()
+    await connection.beginTransaction()
     try {
-      await mysqlPool.query("insert into USER_MASTER set ?", user);
-      const rows = await mysqlPool.query(
+      await connection.query("insert into USER_MASTER set ?", user);
+      const rows = await connection.query(
         "select * from  USER_MASTER where COMPANY_CD = ? and LOGIN_ID = ? ;",
         [user.COMPANY_CD, user.LOGIN_ID]
       );
+      connection.commit()
       res.status(201).json(rows[0]);
     } catch (err) {
       console.log("err: " + err);
+      connection.rollback()
       res.status(500).send(err);
     } finally {
       console.log("create user end.");
+      mysqlPool.releaseConnection(connection);
     }
   })
 
@@ -62,18 +67,23 @@ router
     const companyCode = user.COMPANY_CD;
     const loginId = user.LOGIN_ID;
     const userName = user.USER_NAME;
+    let connection = await mysqlPool.getConnection()
+    await connection.beginTransaction()
     try {
-      const rows = await mysqlPool.query(
+      const rows = await connection.query(
         "update USER_MASTER set COMPANY_CD = ? ,LOGIN_ID = ? ,USER_NAME = ? where COMPANY_CD = ? and LOGIN_ID = ?",
         [companyCode, loginId, userName, companyCode, loginId]
       );
+      connection.commit()
       res.status(200).json(rows[0]);
       // res.status(204).send();
     } catch (err) {
       console.log("err: " + err);
+      connection.rollback()
       res.status(500).send(err);
     } finally {
       console.log("update user end.");
+      mysqlPool.releaseConnection(connection);
     }
   });
 
@@ -82,8 +92,9 @@ router
 // 検索したオブジェクト(配列)を返す
 router.get("/:company_cd/:login_id", async (req, res) => {
   console.log("find by pk start.");
+    let connection = await mysqlPool.getConnection()
   try {
-    const rows = await mysqlPool.query(
+    const rows = await connection.query(
       "select * from  USER_MASTER where COMPANY_CD = ? and LOGIN_ID = ? ;",
       [req.params.company_cd, req.params.login_id]
     );
@@ -97,6 +108,7 @@ router.get("/:company_cd/:login_id", async (req, res) => {
     res.status(500).send(err);
   } finally {
     console.log("find by pk end.");
+    mysqlPool.releaseConnection(connection);
   }
 });
 
@@ -105,17 +117,22 @@ router.get("/:company_cd/:login_id", async (req, res) => {
 // 空データを返す
 router.delete("/:company_cd/:login_id", async (req, res) => {
   console.log("delete user start.");
+  let connection = await mysqlPool.getConnection()
+  await connection.beginTransaction()
   try {
-    await mysqlPool.query(
+    await connection.query(
       "delete from  USER_MASTER where COMPANY_CD = ? and LOGIN_ID = ? ;",
       [req.params.company_cd, req.params.login_id]
     );
+    connection.commit()
     res.status(204).send();
   } catch (err) {
     console.log("err: " + err);
+    connection.rollback()
     res.status(500).send(err);
   } finally {
     console.log("delete user end.");
+    mysqlPool.releaseConnection(connection);
   }
 });
 
