08d8b78442fcf0fbe52d06d15e02a6b473568713
Masatomi KINO <masatomix@ki-no.org>
2019-02-03 01:35:49 +0900

    ESLintのファイル作成 UserMasterは複数のアップに対応

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,19 @@ const app = express()
 
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // 以下、RESTサーバをつくる
 // https://qiita.com/uenosy/items/ba9dbc70781bddc4a491
 
-import userRouter  from './userRouter' 
-import companyRouter  from './companyRouter' 
+import userRouter from './userRouter'
+import companyRouter from './companyRouter'
 
-app.use('/users',userRouter)
-app.use('/companies',companyRouter)
-
-
+app.use('/users', userRouter)
+app.use('/companies', companyRouter)
 
 // Express起動(Firebase Functionで動かす場合はココより上をコピー。)
 const server = app.listen(3000, function() {
-  console.log("Node.js is listening to PORT:" + server.address().port)
+  console.log('Node.js is listening to PORT:' + server.address().port)
 })
--- src/userRouter.ts
+++ src/userRouter.ts
@@ -1,139 +1,150 @@
-import * as express from "express";
-import * as mysql from "promise-mysql";
-import config from "./config";
+import * as express from 'express'
+import * as mysql from 'promise-mysql'
+import config from './config'
 
-const router = express.Router();
+const router = express.Router()
 
-let mysqlPool = mysql.createPool(config);
+let mysqlPool = mysql.createPool(config)
 
 router
   // findAll
   // status 200
   // 結果配列を返す
-  .get("/", async (req, res) => {
-    console.log("find all user start.");
+  .get('/', async (req, res) => {
+    console.log('find all user start.')
     let connection = await mysqlPool.getConnection()
     try {
-      const rows = await connection.query("select * from  USER_MASTER;");
+      const rows = await connection.query('select * from  USER_MASTER')
       if (rows.length === 0) {
-        res.status(404).send();
-        return;
+        res.status(404).send()
+        return
       }
-      res.json(rows);
+      res.json(rows)
     } catch (err) {
-      console.log("err: " + err);
-      res.status(500).send(err);
+      console.log('err: ' + err)
+      res.status(500).send(err)
     } finally {
-      console.log("find all user end.");
-      mysqlPool.releaseConnection(connection);
+      console.log('find all user end.')
+      mysqlPool.releaseConnection(connection)
     }
   })
 
   // create
   // status 201
-  // 作成したオブジェクトを返す
-  .post("/", async (req, res) => {
-    console.log("create user start.");
-    const user = req.body;
-    console.log(JSON.stringify(user));
+  // 作成したオブジェクトを配列で返す
+  .post('/', async (req, res) => {
+    console.log('create user start.')
 
+    let users = req.body
+    console.log(JSON.stringify(users))
+
+    // 登り電文は配列かそうでない場合も受け入れたい。
+    if (Array.isArray(users)) {
+      // なにもしない
+    } else {
+      users = [users] // むりやり配列にしちゃう
+    }
+    let returnArray = []
     let connection = await mysqlPool.getConnection()
     await connection.beginTransaction()
     try {
-      await connection.query("insert into USER_MASTER set ?", user);
-      const rows = await connection.query(
-        "select * from  USER_MASTER where COMPANY_CD = ? and LOGIN_ID = ? ;",
-        [user.COMPANY_CD, user.LOGIN_ID]
-      );
+      for (const user of users) {
+        await connection.query('insert into USER_MASTER set ?', user)
+        let rows = []
+        rows = await connection.query(
+          'select * from  USER_MASTER where COMPANY_CD = ? and LOGIN_ID = ? ;',
+          [user.COMPANY_CD, user.LOGIN_ID]
+        )
+        returnArray.push(rows[0]) // 1件しかヒットしないはず
+      }
       connection.commit()
-      res.status(201).json(rows[0]);
+      res.status(201).send(returnArray)
     } catch (err) {
-      console.log("err: " + err);
+      console.log('err: ' + err)
       connection.rollback()
-      res.status(500).send(err);
+      res.status(500).send(err)
     } finally {
-      console.log("create user end.");
-      mysqlPool.releaseConnection(connection);
+      console.log('create user end.')
+      mysqlPool.releaseConnection(connection)
     }
   })
-
   // update
   // status 200
   // 作成したオブジェクトを返す
-  .put("/", async (req, res) => {
-    console.log("update user start.");
-    const user = req.body;
-    console.log(JSON.stringify(user));
-    const companyCode = user.COMPANY_CD;
-    const loginId = user.LOGIN_ID;
-    const userName = user.USER_NAME;
+  .put('/', async (req, res) => {
+    console.log('update user start.')
+    const user = req.body
+    console.log(JSON.stringify(user))
+    const companyCode = user.COMPANY_CD
+    const loginId = user.LOGIN_ID
+    const userName = user.USER_NAME
     let connection = await mysqlPool.getConnection()
     await connection.beginTransaction()
     try {
       const rows = await connection.query(
-        "update USER_MASTER set COMPANY_CD = ? ,LOGIN_ID = ? ,USER_NAME = ? where COMPANY_CD = ? and LOGIN_ID = ?",
+        'update USER_MASTER set COMPANY_CD = ? ,LOGIN_ID = ? ,USER_NAME = ? where COMPANY_CD = ? and LOGIN_ID = ?',
         [companyCode, loginId, userName, companyCode, loginId]
-      );
+      )
       connection.commit()
-      res.status(200).json(rows[0]);
+      res.status(200).json(rows[0])
       // res.status(204).send();
     } catch (err) {
-      console.log("err: " + err);
+      console.log('err: ' + err)
       connection.rollback()
-      res.status(500).send(err);
+      res.status(500).send(err)
     } finally {
-      console.log("update user end.");
-      mysqlPool.releaseConnection(connection);
+      console.log('update user end.')
+      mysqlPool.releaseConnection(connection)
     }
-  });
+  })
 
 // PK検索
 // status 200
 // 検索したオブジェクト(配列)を返す
-router.get("/:company_cd/:login_id", async (req, res) => {
-  console.log("find by pk start.");
-    let connection = await mysqlPool.getConnection()
+router.get('/:company_cd/:login_id', async (req, res) => {
+  console.log('find by pk start.')
+  let connection = await mysqlPool.getConnection()
   try {
     const rows = await connection.query(
-      "select * from  USER_MASTER where COMPANY_CD = ? and LOGIN_ID = ? ;",
+      'select * from  USER_MASTER where COMPANY_CD = ? and LOGIN_ID = ? ;',
       [req.params.company_cd, req.params.login_id]
-    );
+    )
     if (rows.length === 0) {
-      res.status(404).send();
-      return;
+      res.status(404).send()
+      return
     }
-    res.json(rows);
+    res.json(rows)
   } catch (err) {
-    console.log("err: " + err);
-    res.status(500).send(err);
+    console.log('err: ' + err)
+    res.status(500).send(err)
   } finally {
-    console.log("find by pk end.");
-    mysqlPool.releaseConnection(connection);
+    console.log('find by pk end.')
+    mysqlPool.releaseConnection(connection)
   }
-});
+})
 
 // delete
 // status 204
 // 空データを返す
-router.delete("/:company_cd/:login_id", async (req, res) => {
-  console.log("delete user start.");
+router.delete('/:company_cd/:login_id', async (req, res) => {
+  console.log('delete user start.')
   let connection = await mysqlPool.getConnection()
   await connection.beginTransaction()
   try {
     await connection.query(
-      "delete from  USER_MASTER where COMPANY_CD = ? and LOGIN_ID = ? ;",
+      'delete from  USER_MASTER where COMPANY_CD = ? and LOGIN_ID = ? ;',
       [req.params.company_cd, req.params.login_id]
-    );
+    )
     connection.commit()
-    res.status(204).send();
+    res.status(204).send()
   } catch (err) {
-    console.log("err: " + err);
+    console.log('err: ' + err)
     connection.rollback()
-    res.status(500).send(err);
+    res.status(500).send(err)
   } finally {
-    console.log("delete user end.");
-    mysqlPool.releaseConnection(connection);
+    console.log('delete user end.')
+    mysqlPool.releaseConnection(connection)
   }
-});
+})
 
-export default router;
+export default router
